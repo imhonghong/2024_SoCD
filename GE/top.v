4e3f6ea953c70642851b78db48e19868bf6691fe
Han Lin, Hung <131515657+imhonghong@users.noreply.github.com>
2024-10-24 01:34:18 +0800

    Update top.v

--- GE/top.v
+++ GE/top.v
@@ -1,8 +1,8 @@
 module top(speed, random1, effort, random2, slide, timing, luck3,breakfast, movement, hard, weather, pass3);
-	input [6:0] speed, random1, effort, random2;
+	input [6:0] speed, random1, effort;
 	input [2:0] slide, timing, luck3;
 	input [1:0] breakfast, movement;
-	input [4:0] hard;
+	input [4:0] hard, random2;
 	input weather;
 	output pass3;
 	wire [1:0] bonus1, bonus2;
@@ -38,8 +38,8 @@ endmodule
 module stage2(pass2, bonus2, pass1, bonus1, effort, hard, random2);
 	input pass1;
 	input [1:0] bonus1;
-	input [6:0] effort, random2;
-	input [4:0] hard;
+	input [6:0] effort;
+	input [4:0] hard, random2;
 	output pass2;
 	output [1:0] bonus2;
 	reg [2:0] additional_point;
@@ -55,7 +55,7 @@ module stage2(pass2, bonus2, pass1, bonus1, effort, hard, random2);
 		endcase
 	wire [6:0] score = effort - {2'b00, hard} + {4'b0000, additional_point};
 	wire pass_test = (score >= 7'd70)? 1'b1: 1'b0;
-	wire pass_liver = (effort - {3'b000, bonus1, 2'b00} > 7'd80)? (random2[4] | random2[5]) :1'b1;
+	wire pass_liver = (effort - {3'b000, bonus1, 2'b00} > 7'd80)? (random2[0] | random2[4]) :1'b1;
 	assign pass2 = pass_test & pass_liver & pass1;
 	assign bonus2 = (score > 7'd94)? 2'b11 :(score > 7'd87)? 2'b10 :(score > 7'd80)? 2'b01 : 2'b00;
 endmodule
