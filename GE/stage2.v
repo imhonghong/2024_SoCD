d0cbe1b3a9371559552c4f338b7797323294c22d
Han Lin, Hung <131515657+imhonghong@users.noreply.github.com>
2024-10-23 22:54:30 +0800

    Update stage2.v

--- GE/stage2.v
+++ GE/stage2.v
@@ -9,15 +9,13 @@ module stage2(pass2, bonus2, pass1, bonus1, effort, hard, random2);
 	assign very_hard = hard[4] & 1'b1; //hard>=16
 	assign notso_hard = ~(hard[4] | hard[3] | hard[2]); //hard<3
 	assign medium_hard = ~(very_hard | notso_hard);
-	always@(*) begin
+	wire [1:0] hard_rate = {very_hard, medium_hard};
+	always@(*)
 		case(hard_rate)
-			3'b100: additional_point <= (random2[0] | random2[1])? 3'd8 : 3'd0;
-			3'b010: additional_point <= {random2[3], 1'b0, random2[4]};
+			2'b10: additional_point <= (random2[0] | random2[1])? 3'd8 : 3'd0;
+			2'b01: additional_point <= {random2[3], 1'b0, random2[4]};
 			default: additional_point<= 3'd0;
 		endcase
-		case
-		endcase
-	end
 	wire [6:0] score;
 	assign score = effort - {2'b00, hard} + {4'b0000, additional_point};
 	assign pass_test = (score >= 7'd70)? 1'b1: 1'b0;
