9c13cad0a0bc230e8287bf1d13268c3b5a8f56cb
Han Lin, Hung <131515657+imhonghong@users.noreply.github.com>
2024-10-25 01:10:24 +0800

    Update stage1.v

--- GE/stage1.v
+++ GE/stage1.v
@@ -16,7 +16,7 @@ assign no_parking = random1[4] & random1[2] & random1[0]; //finding no parking s
 assign pass1 = ~(car_accident | no_parking | late); //RT
 always@(*)	//monkey steal your breakfast, breakfast can get a bonus point
 	case(breakfast)
-		2'b01: bonus1 <= (over_speed)? {1'b0,|movement}: {1'b0,^movement};
+		2'b01: bonus1 <= (over_speed)? {1'b0,movement[0]|movement[1]}: {1'b0, movement[0] ^ movement[1] };
 		2'b10: bonus1 <= (over_speed)? {|movement, 1'b0}: (turtle_speed)? {movement[1]&random1[2],1'b0} : {movement[1]^movement[0]^random1[0], 1'b0}; 
 		2'b11: bonus1 <= (movement=={random1[3], random1[5]})? 2'b11: 2'b00;
 		default: bonus1 <= 2'b00;
