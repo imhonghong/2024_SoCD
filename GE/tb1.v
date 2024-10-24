a467440aed1e7ca929879b0cfb80e6293b9eb42c
Han Lin, Hung <131515657+imhonghong@users.noreply.github.com>
2024-10-25 01:09:49 +0800

    Update tb1.v

--- GE/tb1.v
+++ GE/tb1.v
@@ -1,6 +1,6 @@
 `timescale 1ns/100ps
 `include "stage1.v"
-`define pat_len = 524288;
+`define pat_len 524288
 
 module tb1();
 	reg [6:0]	speed, random1;
@@ -8,28 +8,39 @@ module tb1();
 	reg 		weather;
 	wire [1:0]	bonus1;
 	wire	  	pass1;
+
+	reg [18:0] ipt_pattern[0:`pat_len-1] ;
+	reg [2:0] golden_out_pattern[0:`pat_len-1];
+	reg [18:0] fail;
 	
-	reg [18:0] ipt_pattern[0:pat_len-1] ;
-	reg [2:0] golden_out_pattern[0:pat_len-1]; 
-	
-	stage1 g1(.speed(speed), .random1(random1), .breakfast(breakfast), .movement(movement), .weather(weather), 
-			  .bonus1(bonus1), .pass1(pass1));
-	
+	stage1 g1(speed, random1, breakfast, movement, weather, bonus1, pass1);
 	integer i;
+
 	initial begin
-		for (i=0; i<524287; i++) begin
-			{speed, random1, breakfast, movement, weather} = ipt_pattern[i];	// 7,7,2,2,1 bits
+		for (i=0; i<`pat_len; i=i+1) begin
+			{speed,random1,breakfast,movement,weather} <= ipt_pattern[i];
+			if (golden_out_pattern[i] != {bonus1, pass1}) begin
+				fail = fail + 1;
+				$display("Fail at %d golden_out_pattern is %b, ckt output is %b%b, current input is %b", i, golden_out_pattern[i], bonus1,pass1, ipt_pattern[i]);
+			end
 			#10;
 		end
 	end
 	
 	initial begin
-		if ()
+		fail = 19'd0;
+		$readmemb("stage1_input.txt", ipt_pattern);
+		$readmemb("stage1_output.txt", golden_out_pattern);
 	end
-	
-	
+
 	initial begin
-		# ;	$finish;
+		#5242880;
+		if (fail == 0) begin
+			$display("Test passed");
+		end
+		else
+			$display("There're %d fails", fail);
+		$finish;
 	end
 
 endmodule
