06508db848948e0ffae3105ef5bb73d6b09490af
Han Lin, Hung <131515657+imhonghong@users.noreply.github.com>
2024-10-24 09:31:11 +0800

    Update tb1.v

--- GE/tb1.v
+++ GE/tb1.v
@@ -1,1 +1,13 @@
+`timescale 1ns/100ps
+`include "stage1.v"
 module tb1();
+	reg [6:0]	speed, random1;
+	reg [1:0] 	breakfast, movement;
+	reg 		weather;
+	wire [1:0]	bonus1;
+	wire	  	pass1;
+
+	stage1 g1(.speed(speed), .random1(random1), .breakfast(breakfast), .movement(movement), .weather(weather), 
+			  .bonus1(bonus1), .pass1(pass1));
+
+endmodule
