17cf7d25861fcc3c1f1389acec0bda8e8ddc6c75
Han Lin, Hung <131515657+imhonghong@users.noreply.github.com>
2024-10-27 19:00:59 +0800

    Update yin_top.v

--- GE/yin_top.v
+++ GE/yin_top.v
@@ -69,10 +69,10 @@ module stage3(pass3, luck3, slide, timing, bonus2, pass2);
 	reg				bonus3;
 	assign 	fail0 = (slide == 3'd0)? 1'b1 : 1'b0;
 	assign	fail1 = (timing == 3'd0)? 1'b1 : 1'b0;
-	assign	accident0 = ((slide < 3'd3)||(slide == 3'd3))?((slide^(luck3[2:0]) == 3'b000)? ((bonus2==2'd3)? 1'b0 : 1'b1)) : 1'b0;
+	assign	accident0 = (~((slide < 3'd3)||(slide == 3'd3)))? 1'b0: (~(slide^(luck3[2:0]) == 3'b000))? 1'b0 : (bonus2==2'd3)? 1'b0 : 1'b1;
 	assign	accident1 = (slide > 3'd4)? ((slide^(luck3[2:0]) == 3'b001)? 1'b1 : 1'b0): 1'b0;
-	assign 	accident2 = ((timing < 3'd3)||(timing == 3'd3))? ((timing^(luck3[2:0]) == 3'b010)? ((bonus2>=2'd1)?1'b0:1'b1)) : 1'b0;
-	assign	accident3 = ((timing > 3'd4)||(timing == 3'd4))? ((timing^(luck3[2:0]) == 3'b100)? ((bonus2>=2'd2)?1'b0:1'b1)) : 1'b0;
+	assign 	accident2 = (~((timing < 3'd3)||(timing == 3'd3)))? 1'b0: (~(timing^(luck3[2:0]) == 3'b010))? 1'b0: (bonus2>=2'd1)? 1'b0 : 1'b1;
+	assign	accident3 = (~((timing > 3'd4)||(timing == 3'd4)))? 1'b0: (~(timing^(luck3[2:0]) == 3'b100))? 1'b0: (bonus2>=2'd2)? 1'b0 : 1'b1;
 	always(*)begin
 		if((slide>3'd4)&&(accident1==1'b0))
 			bonus3 = 1'b1;
